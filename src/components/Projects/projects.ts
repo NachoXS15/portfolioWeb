88ccd686455df908b09430c81b7481248f6bd4a2
NachoXS-NET <pantojaignacio642@gmail.com>
2024-05-13 22:57:47 -0300

    modified footer icons and started project section

---
+++ src/components/Projects/projects.ts
@@ -0,0 +1,20 @@
+const Projects: object  = {
+    "Password Generator": {
+
+    },
+    "Focus-App": {
+
+    },
+    "Random Selector": {
+
+    },
+    "Feria Del Libro": {
+
+    },
+    "HardTech": {
+
+    },
+    "XSTCH-Logs": {
+        
+    }
+}
