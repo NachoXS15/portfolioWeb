92604c53f9c5818b35e925bd89b7e3fd492e8688
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-07-05 09:42:10 -0300

    added tailwind and updated header

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'purple main': '#242043',
+			'purple-second': '#d4d0e9',
+			'grey': "#919191",
+			"semi-black": "#131111"
+		}
+	},
+	plugins: [],
+}
