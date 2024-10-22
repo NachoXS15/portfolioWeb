4a4c77212607764d476ad4491618977afa239e83
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-10-21 22:53:18 -0300

    created dynamic profile intro

---
+++ src/config/Intro.ts
@@ -0,0 +1,12 @@
+const infodev = [
+    "¡Hola buenas! Bienvenido a mi página web, acá te voy a mostrar un poco de mis trabajos, mis experiencias y mis habilidades.",
+    "Actualmente soy tesista en la carrera de Diseño y Producción Multimedial, ejerzo como Desarrollador Front-end freelancer, realizando trabajos para empresas privadas como para eventos importantes dentro de mi ciudad.",
+    "También me he desarrollado como Project Leader en varios equipos de trabajo."
+]
+const infovideo = [
+    "¡Hola buenas! Bienvenido a mi página web, acá te voy a mostrar un poco de mis trabajos, mis experiencias y mis habilidades.",
+    "Actualmente soy tesista en la carrera de Diseño y Producción Multimedial, ejerzo como Editor de Videos Freelance y realizo trabajos part-time para diversas empresas de mi ciudad, siempre disponible y dispuesto a avanzar en diversos desafios y nuevas oportunidades.",
+    "Me he desarrollado como Project Leader en varios equipos de trabajo."
+]
+
+export {infodev, infovideo}
