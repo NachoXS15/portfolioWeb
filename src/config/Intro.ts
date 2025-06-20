3d8b82b4da2cf7878039d677eb92d1c488be941c
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-06-20 16:44:21 -0300

    fix: updated info

--- src/config/Intro.ts
+++ src/config/Intro.ts
@@ -1,5 +1,5 @@
 const infodev = [
-    "¡Hola! Bienvenido a mi portfolio, aquí mostraré a mostrar un poco de mis trabajos, mis experiencias y mis habilidades.",
+    "¡Hola! Bienvenido a mi portfolio, aquí mostraré un poco de mis trabajos, mis experiencias y mis habilidades.",
     "Actualmente soy tesista en la carrera de Diseño y Producción Multimedial, ejerzo como Desarrollador Front-end freelancer, realizando trabajos para empresas privadas como para eventos importantes dentro de mi ciudad.",
     "También me he desarrollado como Project Leader en varios equipos de trabajo."
 ]
